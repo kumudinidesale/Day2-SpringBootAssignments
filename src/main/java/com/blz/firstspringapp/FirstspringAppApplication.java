c73f0fdf8d482489fdcf7a928444ff4e9184d215
kumudinidesale <kdesale1211@gmail.com>
2022-04-20 16:41:11 +0530

    added:Display the Hello From Blz

---
+++ src/main/java/com/blz/firstspringapp/FirstspringAppApplication.java
@@ -0,0 +1,15 @@
+package com.blz.firstspringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstspringAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstspringAppApplication.class, args);
+        System.out.println("Hello From BridgeLabz:");
+
+    }
+
+}
